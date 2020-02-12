f127ce35fd56afebb297c0e8dc0be7f88cee6380
amelia <sweety_juaner@163.com>
2020-02-12 23:28:58 +0800

    master

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  base: "./",
+  title: "amelia-blog",
+  description: "Vuepress blog demo",
+  themeConfig: {
+    // 你的GitHub仓库，请正确填写
+    repo: "https://github.com/xxxxxxx/blog-demo",
+    // 自定义仓库链接文字。
+    repoLabel: "My GitHub",
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "FirstBlog", link: "/blog/FirstBlog.md" }
+    ],
+    sidebar: [
+      ["/", "首页"],
+      ["/blog/FirstBlog.md", "我的第一篇博客"]
+    ]
+  }
+};
---
+++ docs/blog/FirstBlog.md
@@ -0,0 +1,3 @@
+# 我的第一篇博客
+
+My First Blog
